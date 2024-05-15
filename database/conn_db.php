19a9ab578ab85403f871eeb97cde40cd2f89e33b
KianHe0620 <165177205+KianHe0620@users.noreply.github.com>
2024-05-15 12:05:02 +0800

    connect database & add something to booking module

---
+++ database/conn_db.php
@@ -0,0 +1,12 @@
+<?php
+    $servername = "localhost::3307";
+    $username = "root";
+    $password = "";
+    $dbname = "fK_parking_system";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
