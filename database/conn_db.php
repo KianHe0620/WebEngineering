bedefdd4bf9a4d28a6ec95fd94cf8227ab455511
alyaakmrdn <alyaa.kamaruddin15@gmail.com>
2024-05-22 15:48:04 +0800

    Update conn_db.php

--- database/conn_db.php
+++ database/conn_db.php
@@ -8,8 +8,7 @@ $database = 'fk_parking_system';
 // Server is localhost with
 // port number 3306
 $servername='localhost';
-$mysqli = new mysqli($servername, $user,
-                $password, $database);
+$mysqli = new mysqli($servername, $user, $password, $database);
     
 // Checking for connections
 if ($mysqli->connect_error) {
