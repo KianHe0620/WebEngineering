8f92a9d3204896dd5551ec104e1bb5d09510b3b5
alyaakmrdn <alyaa.kamaruddin15@gmail.com>
2024-06-11 04:27:17 +0800

    Update addparking.php

--- manageParking/addparking.php
+++ manageParking/addparking.php
@@ -74,17 +74,73 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['createparking'])) {
 <body>
 
     <div class="sidebar">
-        <!-- Sidebar content -->
+        <table>
+            <tr>
+                <th>User Profile</th>
+            </tr>
+            <tr>
+                <td><a href="">Content</a></td>
+            </tr>
+            <tr>
+                <td><a href="">Content</a></td>
+            </tr>
+            <tr>
+                <th>Parking Area</th>
+            </tr>
+            <tr>
+                <td><a href="addparking.php">Add Parking</a></td>
+            </tr>
+            <tr>
+                <td><a href="viewparking.php">View Parking</a></td>
+            </tr>
+            <tr>
+                <td><a href="managearea.php">Manage Area</a></td>
+            </tr>
+            <tr>
+                <th>Parking Booking</th>
+            </tr>
+            <tr>
+                <td><a href="/manageBooking/booking.php">Booking</a></td>
+            </tr>
+            <tr>
+                <td><a href="/manageBooking/view.php">View Booking</a></td>
+            </tr>
+            <tr>
+                <th>Traffic Summon</th>
+            </tr>
+            <tr>
+                <td><a href="">Content</a></td>
+            </tr>
+            <tr>
+                <td><a href="">Content</a></td>
+            </tr>
+            <tr>
+                <th><a href="">Log Out</a></th>
+            </tr>
+        </table>
     </div>
 
     <div class="topnav">
-        <!-- Top navigation content -->
+        <div id="menuBtn">&#9776;</div>
+        <div class="logo">
+            <img src="../image/umpsa_logo.png" alt="Logo" width="150" height="50">
+        </div>
+        <div class="search-bar">
+            <form action="" method="">
+                <input type="text" placeholder="Search.." name="search">
+                <button type="submit"><img src="../image/search.png" height="20px" width="20px"></button>
+            </form>
+        </div>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#services">Services</a>
+        <a href="#contact">Contact</a>
     </div>
 
     <div class="content">
         <h2>Create New Parking Area</h2>
         <p>Fill in the information below:</p>
-        <form action="addparking.php" method="POST"> <!-- Corrected form method -->
+        <form action="listparking.php" method="POST"> <!-- Corrected form method -->
             <div class="mb-3">
                 <label for="Parking_area" class="form-label">Parking Area: </label>
                 <select class="form-select" id="Parking_area" name="Parking_area">
