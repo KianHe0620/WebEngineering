1b9d7677f856b3ca2e427ef2545db44efa9116e7
alyaakmrdn <alyaa.kamaruddin15@gmail.com>
2024-06-11 15:11:23 +0800

    Update conn_db.php

--- database/conn_db.php
+++ database/conn_db.php
@@ -2,12 +2,12 @@
 $user = 'root';
 $password = '';
 $database = 'fk_parking_system';
-$servername='localhost';
+$servername = 'localhost';
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$database", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo $e->getMessage();
+    echo "Connection failed: " . $e->getMessage();
 }
 ?>
